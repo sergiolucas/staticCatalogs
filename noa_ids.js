3904c82c43a51e85be98291f36b2f58ef3108871
Sergio Lucas <slucas@aduxia.com>
2025-07-21 22:44:54 +0200

    Upd

--- noa_ids.js
+++ noa_ids.js
@@ -1,6 +1,7 @@
 const ids = [
     {"name": "Bluey", "id": "82728", "type": "series"},
-    {"name": "La casa de Mickey Mouse", "id": "3934", "type": "series"}
+    {"name": "La casa de Mickey Mouse", "id": "3934", "type": "series"},
+    {"name": "Los Tres cerditos", "id": "34463", "type": "movie"},
 ];
 
 module.exports = ids;
